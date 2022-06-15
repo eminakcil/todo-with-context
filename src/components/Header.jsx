8e94fbf40136efac338ecc6a3b9ee2d06c53e8a1
eminakcil <eminakcil@outlook.com>
2022-06-15 21:08:38 +0300

    header buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
 import SignOutButton from './SignOutButton'
 import { useAuth } from '../context'
 import SignInButton from './SignInButton'
+import HeaderButton from './HeaderButton'
 
 export default function Header() {
   const auth = useAuth()
 
   return (
     <div className="flex flex-row-reverse gap-3 mb-6">
-      {Object.keys(auth.user).length === 0 ? <SignInButton /> : <SignOutButton />}
+      {Object.keys(auth.user).length === 0 ? (
+        <SignInButton />
+      ) : (
+        <>
+          <SignOutButton />
+          <HeaderButton className="hover:bg-transparent">{auth.user.username}</HeaderButton>
+        </>
+      )}
     </div>
   )
 }
--- src/components/HeaderButton.jsx
+++ src/components/HeaderButton.jsx
@@ -1,11 +1,15 @@
 import { createElement } from 'react'
 
-export default function HeaderButton({ children, ...props }) {
+export default function HeaderButton({ children, className = '', ...props }) {
   return createElement(
     'button',
     {
-      className:
+      className: [
         'border text-gray-400 border-gray-700 border-t-0 hover:bg-blue-600 hover:border-white hover:text-white px-4 py-2 rounded-b-xl',
+        className,
+      ]
+        .join(' ')
+        .trim(),
       ...props,
     },
     children
