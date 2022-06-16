74047c5fb8da8db1d61ce4f3b4b2f80237a222c2
eminakcil <eminakcil@outlook.com>
2022-06-17 00:07:40 +0300

    todo actions

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,23 +1,58 @@
-import { useAuth } from '../context'
+import classNames from 'classnames'
+import { useAuth, useTodo } from '../context'
 import { trash, check, x } from '../icons'
 
 export default function TodoItem({ todo }) {
   const { user } = useAuth()
+  const { removeTodo, toggleTodo } = useTodo()
+
+  function removeHandle() {
+    removeTodo(todo.id)
+  }
+
+  function toggleHandle() {
+    toggleTodo(todo.id)
+  }
 
   return (
-    <div className="flex flex-row flex-wrap gap-3 justify-between items-center wrapper">
+    <div className="flex flex-col wrapper">
       {/* <pre>{JSON.stringify(todo, null, 2)}</pre> */}
-      <div className="flex-1">{todo.title}</div>
-      {user.id === todo.userId && (
-        <>
-          {todo.complated ? (
-            <button className="form-button btn-outline-danger w-auto">{x}</button>
-          ) : (
-            <button className="form-button btn-outline-success w-auto">{check}</button>
-          )}
-          <button className="form-button btn-outline-danger w-auto">{trash}</button>
-        </>
-      )}
+      <div className="flex flex-row flex-wrap gap-3 justify-between items-center">
+        <div
+          className={classNames({
+            'flex-1': true,
+            'line-through opacity-50': todo.complated,
+          })}
+        >
+          {todo.title}
+        </div>
+        {user.id === todo.userId && (
+          <>
+            {todo.complated ? (
+              <button
+                onClick={toggleHandle}
+                className="form-button btn-outline-danger w-auto"
+              >
+                {x}
+              </button>
+            ) : (
+              <button
+                onClick={toggleHandle}
+                className="form-button btn-outline-success w-auto"
+              >
+                {check}
+              </button>
+            )}
+            <button
+              onClick={removeHandle}
+              className="form-button btn-outline-danger w-auto"
+            >
+              {trash}
+            </button>
+          </>
+        )}
+      </div>
+      {todo.userId !== user.id && <div className="text-sm opacity-30">emin</div>}
     </div>
   )
 }
--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useReducer } from 'react'
+import { nanoid } from 'nanoid'
 
 const TodoContext = createContext()
 
@@ -8,6 +9,20 @@ function reducer(state, action) {
       return {
         todos: [...state.todos, action.payload],
       }
+    case 'REMOVE_TODO':
+      return {
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      }
+    case 'TOGGLE_TODO': {
+      const todoIndex = state.todos.findIndex((todo) => todo.id === action.payload.id)
+      const todos = [...state.todos]
+      todos[todoIndex].complated =
+        action.payload.status === null ? !todos[todoIndex].complated : action.payload.status
+
+      return {
+        todos,
+      }
+    }
     default:
       throw new Error()
   }
@@ -25,7 +40,15 @@ const TodoProvider = ({ children }) => {
   const data = {
     todos: state.todos,
     addTodo(todo) {
+      if (!('id' in todo)) todo.id = nanoid()
+
       dispatch({ type: 'ADD_TODO', payload: todo })
+    },
+    removeTodo(id) {
+      dispatch({ type: 'REMOVE_TODO', payload: id })
+    },
+    toggleTodo(id, status = null) {
+      dispatch({ type: 'TOGGLE_TODO', payload: { id, status } })
     },
   }
 
